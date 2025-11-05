f8e46676efff47effa8b5a0dedcb11b778adc4d8
CowLORD420 <mateijenaru123@gmail.com>
2025-11-05 13:26:29 +0200

    am pus sa creasca exponential sa fie mai usor de controlat la viteze mici urmeaza sa pun si comanda de input mai exact

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveInput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveInput.java
@@ -2,20 +2,28 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+/** sa nu uiti sa pui sa poti sa apesi buton sa fie slowed gen sa faci comanda **/
+
 public class DriveInput {
     private final Gamepad gamepad;
     private double x = 0;
     private double y = 0;
+    private double rot = 0;
     private double cos = 0;
     private double sin = 0;
+    private double epsilon = 0.39;
 
     public DriveInput(Gamepad gamepad){
         this.gamepad = gamepad;
     }
 
     public void update(){
+        rot = gamepad.right_stick_x;
         y = -gamepad.left_stick_y * cos - gamepad.left_stick_x * sin;
         x = -gamepad.left_stick_y * sin + gamepad.left_stick_x * cos;
+        x *= epsilon;
+        y *= epsilon;
+        rot *= epsilon;
     }
 
 
@@ -27,5 +35,5 @@ public class DriveInput {
 
     public double getY(){ return y; }
     public double getX(){ return x; }
-    public double getRx(){ return gamepad.right_stick_x; }
+    public double getRx(){ return rot; }
 }
